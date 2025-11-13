be2715ec3e8a310fbd5d341d3cf9db139928964b
MAKEITWITHMATTY <matthewvaughan1983@googlemail.com>
2025-11-13 15:31:37 +0000

    Create whatisnesting.py
    Day 13

---
+++ Day13/whatisnesting.py
@@ -0,0 +1,11 @@
+# Basic list of dictionaries example
+numbers = [
+    {"id": 1, "label": "One"},
+    {"id": 2, "label": "Two"},
+    {"id": 3, "label": "Three"},
+    {"id": 4, "label": "Four"}
+]
+
+# Access a specific item
+print("\nAccessing a specific element:")
+print(numbers[2]["label"])  # Output: Three
